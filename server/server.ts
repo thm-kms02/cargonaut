e2ca6f75b382ff95e2c6118c4234fed2034a779b
Noah Weber <noah.weber@mni.thm.de>
2021-05-14 13:15:48 +0200

    initial commit

---
+++ server/server.ts
@@ -0,0 +1,32 @@
+import express = require('express');
+import mysql = require('mysql');
+import {Connection, MysqlError} from "mysql";
+import { Request, Response } from 'express';
+
+
+
+const app = express();
+const database : Connection = mysql.createConnection( {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ksm02'
+});
+const basedir: string = __dirname + '/../'
+app.use("/", express.static(basedir + '/client/'));
+app.use("/", express.static(basedir + '/CSS/'));
+app.use("/", express.static(basedir));
+
+app.use(express.json());
+
+app.listen(8080, () => {
+    console.log('Server started at http://localhost:8080');
+});
+
+database.connect( (err: MysqlError) => {
+    if (err) {
+        console.log('Database connection failed: ', err);
+    } else {
+        console.log('Database is connected');
+    }
+});
