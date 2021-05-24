21c81b712c6b50a1cca2685df6a9a62ddfd1f437
Onur <onur.dede@mni.thm.de>
2021-05-24 12:32:55 +0200

    server.ts personenbefoerderung Ansatz

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,7 @@ const database : Connection = mysql.createConnection( {
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'ksm02'
+    database: 'cargo'
 });
 const basedir: string = __dirname + '/../'
 app.use("/", express.static(basedir + '/client/'));
@@ -29,4 +29,33 @@ database.connect( (err: MysqlError) => {
     } else {
         console.log('Database is connected');
     }
+});
+
+app.post('/create/Personenbefoerderung', (req: Request, res: Response) => {
+    const user_ID: string = req.body.user_ID;
+    const anzahlPersonen: string = req.body.anzahlPersonen;
+    const preis: string = req.body.preis;
+    const ang_ges: string = req.body.ang_ges;
+    const start: string = req.body.start;
+    const ziel: string = req.body.ziel;
+    const beschreibung: string = req.body.beschreibung;
+    const bild: string = req.body.beschreibung;
+
+
+    const data = [user_ID, ang_ges, preis ,start,ziel,beschreibung];
+
+        const cQuery: string = "INSERT INTO Anzeige (user_ID, ang_ges, preis, start, ziel, beschreibung ) VALUES (?, ?, ?, ?, ?, ?);";
+        database.query(cQuery, data, (err) => {
+            if (err === null) {
+                res.status(201);
+                res.send(" Anzeige wurde erstellt");
+            } else if (err.errno === 1062) {
+                res.status(500);
+                res.send("Fehler");
+            } else {
+                console.log(err);
+                res.sendStatus(500);
+            }
+        });
+
 });
