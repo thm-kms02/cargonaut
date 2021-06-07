a14d7b21946316985edbb9fe1a2f2317eeaa5520
Onur <onur.dede@mni.thm.de>
2021-06-07 12:52:49 +0200

    Push für Travis-ci versuch 2

---
+++ class/kasse.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Kasse = void 0;
+var Kasse = /** @class */ (function () {
+    function Kasse(user_id, anz_ID, id) {
+        this.id = id;
+        this.user_id = user_id;
+        this.anz_ID = anz_ID;
+    }
+    return Kasse;
+}());
+exports.Kasse = Kasse;
